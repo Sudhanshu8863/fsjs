73e053b116ec99ec914ad7c23c60dbfeb1c21ec3
Sudhanshu Modi <sudhanshu565868@gmail.com>
2023-08-21 08:20:50 +0530

    javascript - array

---
+++ 03_javascript/02_basics/01_arrays.js
@@ -0,0 +1,49 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["Ram", "Hanuman"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+
+// Array methods
+
+// insert value at end
+// myArr.push(6)
+// myArr.push(7)
+// delete value from end
+// myArr.pop()
+// console.log(myArr);
+
+// insert value at start
+// myArr.unshift(9)
+// console.log(myArr);
+
+// remove value from start
+// myArr.shift()
+// console.log(myArr);
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+
+// slice, splice
+
+// console.log("A ", myArr);
+
+// const myn1 = myArr.slice(1, 3)
+
+// console.log(myn1);
+// console.log("B ", myArr);
+
+
+// console.log(myArr);
+// const myn2 = myArr.splice(1, 3)
+// console.log("C ", myArr);
+// console.log(myn2);
