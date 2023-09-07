79c4124d44555afb256349b5b26e695e1a22a79c
Sudhanshu Kumar <sudhanshu565868@gmail.com>
2023-09-07 12:17:22 +0530

    react - 01viteReact showDate

---
+++ 04_react/01viteReact-RockPaperScissor/src/ShowDate.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function ShowDate() {
+  let [date, setDate] = useState(new Date().toLocaleString());
+  setInterval(() => {
+    setDate(() => new Date().toLocaleString());
+  }, 1000);
+
+  return (
+    <h1 className="text-3xl font-semibold px-10 py-5 mt-2 text-stone-50 rounded-lg backdrop-blur-3xl backdrop-brightness-90">
+      {date}
+    </h1>
+  );
+}
+export default ShowDate;
